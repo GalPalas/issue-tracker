794925892032802da5997d59c952e8f0048bc1b1
Gal <galpalas89@gmail.com>
2023-11-29 11:13:37 +0200

    Add additional loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import Skeleton from "react-loading-skeleton";
 import { Table } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import IssueActions from "./IssueActions";
 
